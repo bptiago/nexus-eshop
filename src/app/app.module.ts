777bcb0e7e71207ba1f46d1d47531039597fe1fe
bptiago <125921557+bptiago@users.noreply.github.com>
2024-03-31 21:25:17 -0300

    Started index page

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { IndexComponent } from './pages/index/index.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
